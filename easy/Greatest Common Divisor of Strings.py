5b7036cbb3a4fc62348c46904e8d6e1df0340e0c
Uğur Oğuzhan Obuz <ugurobuz@gmail.com>
2024-02-09 20:05:06 +0300

    correction

--- easy/Greatest Common Divisor of Strings.py
+++ easy/Greatest Common Divisor of Strings.py
@@ -1,22 +1,14 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         s1 = min(str1, str2, key=lambda x: len(x))
-        s2 = max(str1, str2, key=lambda x: len(x))
 
         if str1 == str2:
             return str1
         else:
-            while len(s1) != len(s2):
+            while s1:
                 if len(str1) / len(s1) == str1.count(s1) and len(str2) / len(s1) == str2.count(s1):
                     return s1
                 else:
                     s1 = s1[:-1]
 
             return ""
-
-
-
-
-str1 ="LEET"
-str2 ="CODE"
-print(str1.count(str2))
